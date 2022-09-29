c977ead49792f3d0f56d99075bb3a6511e2218cd
antonbergit <anton.berg.it@gmail.com>
2022-09-29 08:45:59 +0300

    manifest version correction

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     'maintainer': 'SPOC corp',
     'website': "https://spoc-odoo.com.ua",
     'category': 'Productivity',
-    'version': '8.0',
+    'version': '15.0.1.8.0',
 
     'depends': ['base', 'hr_timesheet'],
 
